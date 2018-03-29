ab1abba88c0b87fee28ff3945a6b9fa148fe055c
Rodion Mescheryakov <msrodion@gmail.com>
2018-03-29 21:47:37 +0300

    Add `id` fields for all entities. Expand nested entities into full form rather than having `id` field only

--- main/recipes/serializers.py
+++ main/recipes/serializers.py
@@ -1,32 +1,44 @@
-from .models import Recipe, Category, Cuisine, Ingredient
 from rest_framework import serializers
+from .models import Recipe, Category, Cuisine, Ingredient
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+
+
+class CuisineSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Cuisine
+        fields = ('id', 'name')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+    category = CategorySerializer()
+    cuisine = CuisineSerializer()
+    ingredients = IngredientSerializer(many=True)
+
     class Meta:
         model = Recipe
         fields = (
+            'id',
             'name',
             'category',
             'cuisine',
             'description',
             'ingredients',
         )
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('name',)
-
-
-class CuisineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cuisine
-        fields = ('name',)
-
-
-class IngredientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ingredient
-        fields = ('name',)
