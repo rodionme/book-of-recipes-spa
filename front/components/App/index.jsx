c97336e130119fc30f83b8fe19fca3013b02e60d
Rodion Mescheryakov <msrodion@gmail.com>
2018-03-31 20:35:33 +0300

    Initialize React app with Webpack and Babel

---
+++ front/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './style.css';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <span>It works!</span>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+
+module.exports = {
+  mode: 'production',
+  entry: './front/app.js',
+  output: {
+    path: `${__dirname}/main/recipes/static/recipes`,
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'front'),
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      }
+    ]
+  },
+};
