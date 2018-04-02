d9818b824fd77f248b7a45ffbbc57c0927b858e9
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-02 21:10:08 +0300

    Update RecipeViewSet with queries

--- main/recipes/views.py
+++ main/recipes/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.db.models import Count
 from rest_framework.viewsets import ModelViewSet
 from .serializers import RecipeSerializer, CategorySerializer, CuisineSerializer, IngredientSerializer
 from .models import Recipe, Ingredient, Cuisine, Category
@@ -11,6 +12,25 @@ class IndexView(TemplateView):
 class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+
+    def get_queryset(self):
+        recipes = Recipe.objects.all()
+        query = self.request.GET.get('q')
+        ingredients = self.request.GET.get('i')
+
+        if query:
+            recipes = recipes.filter(name__contains=query)
+
+        if ingredients:
+            ingredients = [x for x in ingredients.split(',')]
+            recipes = recipes \
+                .filter(ingredients__pk__in=ingredients) \
+                .annotate(ingredients_count=Count('ingredients')) \
+                .order_by('-ingredients_count')
+
+            # TODO: Implement additional ascending sorting by number of missing ingredients
+
+        return recipes
 
 
 class CategoryViewSet(ModelViewSet):
