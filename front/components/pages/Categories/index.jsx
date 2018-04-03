56ae1a25170b5254ad6861988e6b672e6037fd7a
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-03 20:48:57 +0300

    Implement `Categories` page

--- front/components/pages/Categories/index.jsx
+++ front/components/pages/Categories/index.jsx
@@ -5,7 +5,26 @@ import Header from '../../Header'
 
 
 export default class Categories extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/categories/')
+      .then(({data: categories} = response) => {
+        this.setState({
+          categories
+        });
+      });
+  }
+
   render() {
+    let categories = this.state.categories;
+
     return (
       <React.Fragment>
         <Header onSearchSubmit={this.onSearchSubmit}/>
@@ -13,6 +32,20 @@ export default class Categories extends React.Component {
         <div className="divider divider-2"/>
 
         <section className="content">
+          <main className="main-content">
+            {categories.length ? (
+              <ul>
+                {categories.map(category => {
+                  return <li key={category.id}><a href={category.id}>{category.name}</a></li>
+                })}
+              </ul>
+            )
+            :
+              <p>No categories are available.</p>
+            }
+
+            <a href="/add-category">Add new category</a>
+          </main>
         </section>
       </React.Fragment>
     );
