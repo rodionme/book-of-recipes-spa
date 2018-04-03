82b59133730cf7e2a08abc488923ce743d28fae6
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-03 20:49:21 +0300

    Implement `Cuisines` page

--- front/components/pages/Cuisines/index.jsx
+++ front/components/pages/Cuisines/index.jsx
@@ -5,7 +5,26 @@ import Header from '../../Header'
 
 
 export default class Cuisines extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cuisines: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/cuisines/')
+      .then(({data: cuisines} = response) => {
+        this.setState({
+          cuisines
+        });
+      });
+  }
+
   render() {
+    let cuisines = this.state.cuisines;
+
     return (
       <React.Fragment>
         <Header onSearchSubmit={this.onSearchSubmit}/>
@@ -13,6 +32,20 @@ export default class Cuisines extends React.Component {
         <div className="divider divider-2"/>
 
         <section className="content">
+          <main className="main-content">
+            {cuisines.length ? (
+              <ul>
+                {cuisines.map(cuisine => {
+                  return <li key={cuisine.id}><a href={cuisine.id}>{cuisine.name}</a></li>
+                })}
+              </ul>
+            )
+            :
+              <p>No cuisines are available.</p>
+            }
+
+            <a href="/add-cuisine">Add new cuisine</a>
+          </main>
         </section>
       </React.Fragment>
     );
