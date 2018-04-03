56a318e908741152346aa7352e06531c624e3ff1
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-03 22:13:33 +0300

    Implement `Category` page view

---
+++ front/components/pages/Category/index.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import axios from 'axios';
+import './style.css';
+import Header from '../../Header'
+
+
+export default class Category extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      category: null,
+      categoryId: null,
+    };
+
+    this.deleteCategory = this.deleteCategory.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({
+      categoryId: this.props.match.params.categoryId,
+    }, () => {
+      axios.get(`/api/categories/${this.state.categoryId}/`)
+      .then(({data: category} = response) => {
+        this.setState({
+          category
+        });
+      });
+    });
+  }
+
+  deleteCategory(e) {
+    e.preventDefault();
+
+    axios.delete(`/api/categories/${this.state.categoryId}/`)
+      .then(() => {
+        this.props.history.push(`/categories`);
+      });
+  }
+
+  render() {
+    let category = this.state.category;
+
+    return (
+      <React.Fragment>
+        <Header noSearch/>
+
+        <div className="divider divider-2"/>
+
+        <section className="content">
+          <main className="main-content">
+            {category && (
+              <React.Fragment>
+                <p>Name: {category.name}</p>
+
+                <a href={`/categories/${category.id}/edit`}>Edit</a>
+                <a href="" onClick={this.deleteCategory}>Delete</a>
+              </React.Fragment>
+            )}
+          </main>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
