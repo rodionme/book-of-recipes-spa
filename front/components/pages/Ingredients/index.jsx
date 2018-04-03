d443cda35e7881bbf082625d5944d509b35d0093
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-03 20:51:39 +0300

    Implement `Ingredients` page

--- front/components/pages/Ingredients/index.jsx
+++ front/components/pages/Ingredients/index.jsx
@@ -5,7 +5,26 @@ import Header from '../../Header'
 
 
 export default class Ingredients extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ingredients: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/ingredients/')
+      .then(({data: ingredients} = response) => {
+        this.setState({
+          ingredients
+        });
+      });
+  }
+
   render() {
+    let ingredients = this.state.ingredients;
+
     return (
       <React.Fragment>
         <Header onSearchSubmit={this.onSearchSubmit}/>
@@ -13,6 +32,20 @@ export default class Ingredients extends React.Component {
         <div className="divider divider-2"/>
 
         <section className="content">
+          <main className="main-content">
+            {ingredients.length ? (
+              <ul>
+                {ingredients.map(ingredient => {
+                  return <li key={ingredient.id}><a href={ingredient.id}>{ingredient.name}</a></li>
+                })}
+              </ul>
+            )
+            :
+              <p>No ingredients are available.</p>
+            }
+
+            <a href="/add-ingredient">Add new ingredient</a>
+          </main>
         </section>
       </React.Fragment>
     );
