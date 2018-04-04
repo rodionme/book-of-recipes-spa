a859b5530b660c71dc50f6adbb14f9b3c5719df2
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-04 12:04:13 +0300

    Update routes

--- front/components/Router.jsx
+++ front/components/Router.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Recipes from './pages/Recipes';
+import EditRecipe from './pages/EditRecipe';
 import Recipe from './pages/Recipe';
 import Ingredients from './pages/Ingredients';
+import EditIngredient from './pages/EditIngredient';
 import Ingredient from './pages/Ingredient';
 import Cuisines from './pages/Cuisines';
+import EditCuisine from './pages/EditCuisine';
 import Cuisine from './pages/Cuisine';
 import Categories from './pages/Categories';
+import EditCategory from './pages/EditCategory';
 import Category from './pages/Category';
 import NotFound from './pages/NotFound';
 
@@ -17,22 +21,22 @@ const Router = () => (
       <Route exact path="/" component={Recipes} />
 
       <Route exact path="/recipes" component={Recipes}/>,
-      {/*<Route exact path="recipes/add" component={AddRecipe}/>,*/}
+      <Route exact path="/recipes/add" component={EditRecipe}/>,
       <Route exact path="/recipes/:recipeId" component={Recipe}/>,
       {/*<Route exact path="recipes/:recipeId/edit" component={EditRecipe}/>,*/}
 
       <Route exact path="/ingredients" component={Ingredients}/>,
-      {/*<Route exact path="/ingredients/add" component={AddIngredient}/>,*/}
+      <Route exact path="/ingredients/add" component={EditIngredient}/>,
       <Route exact path="/ingredients/:ingredientId" component={Ingredient}/>,
       {/*<Route exact path="/ingredients/:ingredientId/edit" component={EditIngredient}/>,*/}
 
       <Route exact path="/cuisines" component={Cuisines}/>,
-      {/*<Route exact path="cuisines/add" component={AddCuisine}/>,*/}
+      <Route exact path="/cuisines/add" component={EditCuisine}/>,
       <Route exact path="/cuisines/:cuisineId" component={Cuisine}/>,
       {/*<Route exact path="cuisines/:cuisineId/edit" component={EditCuisine}/>,*/}
 
       <Route exact path="/categories" component={Categories}/>,
-      {/*<Route exact path="categories/add" component={AddCategory}/>,*/}
+      <Route exact path="/categories/add" component={EditCategory}/>,
       <Route exact path="/categories/:categoryId" component={Category}/>,
       {/*<Route exact path="categories/:categoryId/edit" component={EditCategory}/>,*/}
 
