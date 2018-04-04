8eabf6cb08409e67cf0d150a96878761b2314914
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-04 12:01:39 +0300

    Implement `EditIngredient` page view

---
+++ front/components/pages/EditIngredient/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import './style.css';
+import Header from '../../Header'
+
+
+export default class EditIngredient extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    axios.post(`/api/ingredients/`, {
+      name: this.state.name,
+    })
+      .then(() => {
+        this.props.history.push(`/ingredients`);
+      });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Header noSearch/>
+
+        <div className="divider divider-2"/>
+
+        <section className="content">
+          <main className="main-content">
+            <form action="" method="post" onSubmit={this.handleSubmit}>
+              <p>Name: <input name="name" onChange={this.handleChange} value={this.state.name} type="text" placeholder="Name" /></p>
+
+              <button type="submit">Save</button>
+            </form>
+          </main>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+EditIngredient.propTypes = {
+  name: PropTypes.string,
+};
