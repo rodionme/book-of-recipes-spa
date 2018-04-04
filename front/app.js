8873467ebffc9806958a48bec235f389cdea801a
Rodion Mescheryakov <msrodion@gmail.com>
2018-04-04 12:03:38 +0300

    Set default headers for POST requests

--- front/app.js
+++ front/app.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
+import axios from 'axios';
+import { getCookie } from './helpers';
 
+
+axios.defaults.headers.post['X-XSRF-TOKEN'] = getCookie('csrftoken');
 
 ReactDOM.render(<Router />, document.getElementById('app'));
